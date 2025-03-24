2a46907c405050329d33db4c908e5f83977de762
Maciej Krüger <mkg20001@gmail.com>
2025-03-24 23:20:54 +0100

    flake: add

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "NixOS for Android Terminal (Android Virtualization Framework) ";
+
+  outputs = { self }: {
+    nixosModules = {
+      avf = import ./avf;
+      avfDebug = import ./avf/debug.nix;
+    };
+  };
+}
