1e3e9c00a214cd6d8b7cadc8ee379c630bb564cf
Maciej Krüger <mkg20001@gmail.com>
2025-03-25 03:35:43 +0100

    feat: add cache

--- avf/default.nix
+++ avf/default.nix
@@ -176,6 +176,13 @@ with lib;
       vm_config = vmConfig.generate "vm_config.json" cfg.vmConfig;
     };
 
+    nix.settings = {
+      trusted-substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
     boot.growPartition = true;
     boot.loader.systemd-boot.enable = true;
     boot.initrd.systemd.enable = true;
