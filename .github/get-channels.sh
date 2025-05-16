39893e7cdea1e3942b2e83e95b43526b14a36eb9
Maciej Krüger <mkg20001@gmail.com>
2025-05-16 14:13:00 +0200

    fix: ci

--- .github/get-channels.sh
+++ .github/get-channels.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl gnugrep gnused jq
 
-matrix=$(curl -L -s https://github.com/NixOS/infra/raw/refs/heads/main/channels.nix | grep -o '"nixos-[0-9.a-z]*"' | sed 's|"||g' | sed "s|nixos-||g" | jq -s -R -c 'split("\n") | map(select(length > 0))')
+matrix=$(curl -L -s https://github.com/NixOS/infra/raw/refs/heads/main/channels.nix | grep -o '"nixos-[0-9.a-z]*"' | sed 's|"||g' | sed "s|nixos-||g" | grep -v 24.05 | jq -s -R -c 'split("\n") | map(select(length > 0))')
 echo "matrix=$matrix" >> $GITHUB_OUTPUT
